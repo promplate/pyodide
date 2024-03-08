ed9d6aaadeade60f0f44ab73cb109528bc0b7779
Muspi Merol <wjj123345567@icloud.com>
2024-03-08 21:59:09 +0800

    fix: make `WarnPrinter` more robust

--- src/promplate_pyodide/utils/warn.py
+++ src/promplate_pyodide/utils/warn.py
@@ -8,8 +8,14 @@ class WarnPrinter:
     def __await__(self):
         return self
 
-    def __repr__(self) -> str:
+    def __repr__(self):
         return self.warn
+
+    def __getattr__(self, _):
+        return self
+
+    def __getitem__(self, _):
+        return self
 
 
 NotImplementedWarning = WarnPrinter("This feature does not support pyodide runtime!")
