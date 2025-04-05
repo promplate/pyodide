516915215e6f78c6108abac1e77b438314a62088
Muspi Merol <me@promplate.dev>
2025-04-06 03:45:23 +0800

    fix: patch v0 api only when openai v0 is installed

--- src/promplate_pyodide/utils/openai/sync.py
+++ src/promplate_pyodide/utils/openai/sync.py
@@ -4,14 +4,20 @@ from ..sync import to_sync
 
 
 def patch_sync_apis():
-    from promplate.llm.openai import v0, v1
+    from openai.version import VERSION
 
-    v1.SyncTextOpenAI.complete = v1.TextComplete.__call__ = to_sync(v1.AsyncTextComplete.__call__)
-    v1.SyncTextOpenAI.generate = v1.TextGenerate.__call__ = to_sync(v1.AsyncTextGenerate.__call__)
-    v1.SyncChatOpenAI.complete = v1.ChatComplete.__call__ = to_sync(v1.AsyncChatComplete.__call__)
-    v1.SyncChatOpenAI.generate = v1.ChatGenerate.__call__ = to_sync(v1.AsyncChatGenerate.__call__)
+    if VERSION.startswith("0"):
+        from promplate.llm.openai import v0
 
-    v0.TextComplete.__call__ = to_sync(v0.AsyncTextComplete.__call__)
-    v0.TextGenerate.__call__ = to_sync(v0.AsyncTextGenerate.__call__)
-    v0.ChatComplete.__call__ = to_sync(v0.AsyncChatComplete.__call__)
-    v0.ChatGenerate.__call__ = to_sync(v0.AsyncChatGenerate.__call__)
+        v0.TextComplete.__call__ = to_sync(v0.AsyncTextComplete.__call__)
+        v0.TextGenerate.__call__ = to_sync(v0.AsyncTextGenerate.__call__)
+        v0.ChatComplete.__call__ = to_sync(v0.AsyncChatComplete.__call__)
+        v0.ChatGenerate.__call__ = to_sync(v0.AsyncChatGenerate.__call__)
+
+    else:
+        from promplate.llm.openai import v1
+
+        v1.SyncTextOpenAI.complete = v1.TextComplete.__call__ = to_sync(v1.AsyncTextComplete.__call__)
+        v1.SyncTextOpenAI.generate = v1.TextGenerate.__call__ = to_sync(v1.AsyncTextGenerate.__call__)
+        v1.SyncChatOpenAI.complete = v1.ChatComplete.__call__ = to_sync(v1.AsyncChatComplete.__call__)
+        v1.SyncChatOpenAI.generate = v1.ChatGenerate.__call__ = to_sync(v1.AsyncChatGenerate.__call__)
