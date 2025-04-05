f31d4ed19db6673a902a53be84520a4cc9b3e9b5
Muspi Merol <me@promplate.dev>
2025-04-05 21:06:04 +0800

    chore: remove redundant reassignments

--- src/promplate_pyodide/utils/openai/sync.py
+++ src/promplate_pyodide/utils/openai/sync.py
@@ -8,10 +8,3 @@ def patch_sync_apis():
     o.SyncTextOpenAI.generate = o.TextGenerate.__call__ = to_sync(o.AsyncTextGenerate.__call__)  # type: ignore
     o.SyncChatOpenAI.complete = o.ChatComplete.__call__ = to_sync(o.AsyncChatComplete.__call__)  # type: ignore
     o.SyncChatOpenAI.generate = o.ChatGenerate.__call__ = to_sync(o.AsyncChatGenerate.__call__)  # type: ignore
-
-    o.v1.TextComplete = o.TextComplete
-    o.v1.TextGenerate = o.TextGenerate
-    o.v1.ChatComplete = o.ChatComplete
-    o.v1.ChatGenerate = o.ChatGenerate
-    o.v1.SyncTextOpenAI = o.SyncTextOpenAI
-    o.v1.SyncChatOpenAI = o.SyncChatOpenAI
